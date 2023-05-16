c2c69b9e9e1468c4a72af7fedf03c4b028a2d749
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-16 18:14:01 +0600

    data loadede from mongodb

--- src/loaders/cartProducts.js
+++ src/loaders/cartProducts.js
@@ -1,14 +1,14 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const cartProducts = async() => {
-  const loadedProducts = await fetch('products.json');
+  const loadedProducts = await fetch('http://localhost:5000/products');
   const products = await loadedProducts.json();
   console.log(products)
   // load cart products from shoppingcart local storage
   const storedCart = getShoppingCart();
   const savedCart = [];
   for(const id in storedCart){
-    const addedProduct = products.find(pd => pd.id === id);
+    const addedProduct = products.find(pd => pd._id === id);
     
 
     if(addedProduct){
